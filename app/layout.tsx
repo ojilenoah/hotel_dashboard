d127b11b630fa8399e4615d8442b8fe43ddb3351
Noah Ojile <noahojile04@gmail.com>
2025-04-27 23:04:09 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'trippytrip',
+  title: 'Hotel_Dashboard_TripyTrip',
   description: 'Created with v0',
   generator: 'NOah Ojile',
 }
